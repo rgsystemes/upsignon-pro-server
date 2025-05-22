5c118aed56956ba824b6b6f362f0628e25e937fc
Gireg <giregk@upsignon.eu>
2025-05-22 10:09:26 +0200

    wip

--- src/helpers/randomString.ts
+++ src/helpers/randomString.ts
@@ -1,6 +1,16 @@
 export const getRandomString = (length: number): string => {
   let result = '';
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  // Do not allow
+  /**
+   * DO NOT ALLOW
+   * - O (big o)
+   * - 0
+   * - l
+   * - I
+   * - 1
+   * To avoid possible confusions when displaying codes in emails.
+   */
+  const alphabet = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789+&@%!';
   const alphabetLength = alphabet.length;
   for (let i = 0; i < length; i++) {
     result += alphabet.charAt(Math.floor(Math.random() * alphabetLength));
