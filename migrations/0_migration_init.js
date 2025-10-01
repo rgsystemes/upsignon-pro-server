247cfaf134af022ca4f2ea462c8e1cad2ff580aa
giregk <giregk@users.noreply.github.com>
2025-10-01 16:11:52 +0200

    fix install by creating admin_sessions table in migrations

--- migrations/0_migration_init.js
+++ migrations/0_migration_init.js
@@ -1,9 +1,13 @@
-exports.up = function (db) {
-  return db.query(
+exports.up = async function (db) {
+  await db.query(
     'CREATE TABLE IF NOT EXISTS migrations (name varchar(255) NOT NULL, migration_time timestamp with time zone DEFAULT current_timestamp(0))',
+  );
+  await db.query(
+    'CREATE TABLE IF NOT EXISTS admin_sessions (session_id VARCHAR PRIMARY KEY, session_data JSONB NOT NULL, expiration_time TIMESTAMP(0) NOT NULL)',
   );
 };
 
-exports.down = function (db) {
-  return db.query('DROP TABLE migrations');
+exports.down = async function (db) {
+  await db.query('DROP TABLE migrations');
+  await db.query('DROP TABLE admin_sessions');
 };
