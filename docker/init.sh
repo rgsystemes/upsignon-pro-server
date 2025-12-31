7373f01eebd56a38a754eb1af89dc1de56786e8f
f-fatien <floriane.fatien@rgsystem.com>
2025-12-31 10:32:13 +0100

    chore-869: Update script

--- docker/init.sh
+++ docker/init.sh
@@ -2,10 +2,10 @@
 
 # Requirements
 if [ $EUID -ne 0 ]; then
-  echo "Please run the script as root..."
+  echo "⚠️  Please run the script as root..."
   exit 1
 elif [ ! -d "/var/lib/docker" ]; then
-  echo "Docker is not installed. Please install Docker before running this script..."
+  echo "⚠️  Docker is not installed. Please install Docker before running this script..."
   exit 1
 fi
 
@@ -17,8 +17,8 @@ if [[ $1 == "--help" ]] || [[ $1 == "-h" ]]; then
   echo "Make sure Docker is installed and running before executing this script."
   echo "Options:"
   echo "  -h, --help    Show this help message and exit"
-  echo "  -le           [MANDATORY] Specify to use Let's Encrypt for TLS certificates"
-  echo "  -certs        [MANDATORY] Specify to use custom TLS certificates"
+  echo "  -le           [REQUIRED] Specify to use Let's Encrypt for TLS certificates"
+  echo "  -certs        [REQUIRED] Specify to use custom TLS certificates"
   exit 0
 elif [[ $1 == "-le" ]]; then
   echo "Using Let's Encrypt for TLS certificates..."
@@ -44,33 +44,35 @@ if [[ $SSL == "certs" ]]; then
     CERT_FILE=$SSL/tls.yml
     echo -e "tls:\n  certificates:" > $CERT_FILE
 
+    # Add each certificate and its corresponding key to the TLS configuration
     for cert in $CERTS; do
       key="${cert%.crt}.key"; if [[ -f $key ]]; then
         echo "    - certFile: /$cert" >> $CERT_FILE
         echo "      keyFile: /$key" >> $CERT_FILE
       else
-        echo "⚠️ No $key file found in the $SSL directory. Please add your private key before proceeding. Script stopped."
+        echo "⚠️  No $key file found in the $SSL directory. Please add your private key before proceeding. Script stopped."
         exit 1
       fi
     done
     echo "✅ Traefik TLS configuration generated at $CERT_FILE"
   else
-    echo "⚠️ No .crt files found in the $SSL directory. Please add your TLS certificates before proceeding. Script stopped."
+    echo "⚠️  No .crt files found in the $SSL directory. Please add your TLS certificates before proceeding. Script stopped."
     exit 1
   fi
 fi
 
 # Start UpsignOn by Septeo
-echo "Start UpsignOn by Septeo..."
+echo "🚀 Start UpsignOn by Septeo..."
 docker compose -f docker-compose-$SSL.yml up -d
 
-echo "Initializing the application..."
+# Wait for the application to be ready
+echo "⏳ Initializing the application..."
 source .env
 while ! docker logs uso.dashboard 2>&1 | grep -q "port: $DASHBOARD_PORT"; do
-    echo "Waiting for the application to start ..."
-    sleep 20
+    echo "⏳ Waiting for the application to start ..."
+    sleep 30
 done
 
 # Create the Super Admin
 SA_URL=$(docker exec -it uso.dashboard node /app/upsignon-pro-dashboard/back/scripts/addSuperAdmin.js | tail -n 1)
-echo "Super Admin has been created successfully. The URL is valid for 5 minutes : $SA_URL"
+echo "✅ Super Admin has been created successfully. The URL is valid for 5 minutes : $SA_URL"
