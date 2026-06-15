981243b2644ea8f1400edac892917b7a61174979
Gireg <gireg.dekerdanet@septeo.com>
2026-06-15 09:53:03 +0200

    11460 completely remove brank name IDOR

--- tests/integration/shamirRecovery/getShamirConfigs.test.ts
+++ tests/integration/shamirRecovery/getShamirConfigs.test.ts
@@ -50,7 +50,6 @@ const mockCheckBasicAuth2Success = (userId: number) => {
     bankIds: {
       publicId: b.public_id,
       internalId: b.id,
-      usesDeprecatedIntId: false,
     },
   });
 };
